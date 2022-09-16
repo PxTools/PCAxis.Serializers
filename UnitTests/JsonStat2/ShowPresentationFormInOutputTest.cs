a2094e51dee837ba31f401521736a40e3cf119b7
JohannesFinsveen <56305961+JohannesFinsveen@users.noreply.github.com>
2022-09-16 15:15:59 +0200

    Added new px-file which uses PRESTEXT keyword, but/and discovered we uses different numbers for the same thing, see PR

--- UnitTests/JsonStat2/ShowPresentationFormInOutputTest.cs
+++ UnitTests/JsonStat2/ShowPresentationFormInOutputTest.cs
@@ -35,20 +35,38 @@ namespace UnitTests.JsonStat2
             System.Threading.Thread.CurrentThread.CurrentCulture = ci;
             System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
                 
-            PXModel myModel = helper.GetSelectAllModel("TestFiles/BE0101A1_20200914-143936.px");
+            PXModel myModel = helper.GetSelectAllModel("TestFiles/BE0101A1_show_codes_or.px");
             
-            myModel.Meta.Variables.Where(x => x.PresentationText == 1 && x.Code == "region")
-                .ToList().ForEach(y => y.PresentationText = 3);
+           // myModel.Meta.Variables.Where(x => x.PresentationText == 1 && x.Code == "region")
+  //              .ToList().ForEach(y => y.PresentationText = 3);
             
             var actual = helper.GetActual(myModel);
          
             var jsonstat2Object = JObject.Parse(actual);
-         
-            var showValueSex = jsonstat2Object["dimension"]["sex"]["extension"]["show"].ToString();
-            Assert.AreEqual("code",showValueSex);
+   
+            /* From px-file_format_specification_2013.pdf
+             The values text should always be defined by the VALUES keyword and the codes should always be defined by the CODES keyword.
+               0 - Only the value code should be displayed.
+               1 - Only the value text should be displayed.
+               2 - Both code and value should be displayed and the order should be the code and then the value text.
+               3 - Both code and value should be displayed and the order should be the value text then the value code
+             */
 
+            //PX:No mention, should defalt to 1 
+            var showValueSex = jsonstat2Object["dimension"]["sex"]["extension"]["show"].ToString();
+            Assert.AreEqual("text",showValueSex,"For var=sex");
+
+            //PX: =2
+            var showValueMaritalStatus = jsonstat2Object["dimension"]["marital status"]["extension"]["show"].ToString();
+            Assert.AreEqual("code_value", showValueMaritalStatus, "For var=marital status");
+
+            //PX: =3
             var showValueRegion = jsonstat2Object["dimension"]["region"]["extension"]["show"].ToString();
-            Assert.AreEqual("code_value", showValueRegion);
+            Assert.AreEqual("value_code", showValueRegion, "For var=region");
+
+          
+
+
         }
 
     }
