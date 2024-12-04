2024497f799505d7950f9f98fd860e6074b2fff5
likp <petros.likidis@scb.se>
2024-12-04 08:38:38 +0100

    Uncomment some tests

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -12,49 +12,49 @@ class Program
         builder.BuildForPresentation(selection);
         var model = builder.Model;
 
-        //// Serialize to Parquet
-        //using (FileStream stream = new FileStream(new Uri(@"../../../../UnitTests/TestFiles/TestParquet.parquet", UriKind.Relative).ToString(), FileMode.Create))
-        //{
-        //    var parquetSer = new ParquetSerializer();
-        //    try
-        //    {
-        //        parquetSer.Serialize(model, stream);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.ToString());
-        //    }
-        //}
+        // Serialize to Parquet
+        using (FileStream stream = new FileStream(new Uri(@"../../../../UnitTests/TestFiles/TestParquet.parquet", UriKind.Relative).ToString(), FileMode.Create))
+        {
+            var parquetSer = new ParquetSerializer();
+            try
+            {
+                parquetSer.Serialize(model, stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
 
-        //// Serialize to XLSX
-        //using (FileStream stream = new FileStream(new Uri(@"../../../../UnitTests/TestFiles/TestExcel.xlsx", UriKind.Relative).ToString(), FileMode.Create))
-        //{
-        //    var ser = new XlsxSerializer();
-        //    ser.DoubleColumn = DoubleColumnType.NoDoubleColumns;
-        //    ser.InformationLevel = InformationLevelType.AllInformation;
-        //    try
-        //    {
-        //        ser.Serialize(model, stream);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.ToString());
-        //    }
-        //}
+        // Serialize to XLSX
+        using (FileStream stream = new FileStream(new Uri(@"../../../../UnitTests/TestFiles/TestExcel.xlsx", UriKind.Relative).ToString(), FileMode.Create))
+        {
+            var ser = new XlsxSerializer();
+            ser.DoubleColumn = DoubleColumnType.NoDoubleColumns;
+            ser.InformationLevel = InformationLevelType.AllInformation;
+            try
+            {
+                ser.Serialize(model, stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
 
-        //// Serialize to JSON
-        //using (FileStream stream = new FileStream(new Uri(@"../../../../UnitTests/TestFiles/TestJson.json", UriKind.Relative).ToString(), FileMode.Create))
-        //{
-        //    var ser = new JsonSerializer();
-        //    try
-        //    {
-        //        ser.Serialize(model, stream);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.ToString());
-        //    }
-        //}
+        // Serialize to JSON
+        using (FileStream stream = new FileStream(new Uri(@"../../../../UnitTests/TestFiles/TestJson.json", UriKind.Relative).ToString(), FileMode.Create))
+        {
+            var ser = new JsonSerializer();
+            try
+            {
+                ser.Serialize(model, stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
 
         // Serialize to CSV
         using (FileStream stream = new FileStream(new Uri(@"../../../../UnitTests/TestFiles/TestCsv.csv", UriKind.Relative).ToString(), FileMode.Create))
