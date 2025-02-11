17c223bd0ba25198cf18b51c02c843243177d875
trygu <trygve.falch@gmail.com>
2025-02-11 15:59:51 +0100

    Reduced cognitive complexity

--- PCAxis.Serializers/Parquet/ParquetBuilder.cs
+++ PCAxis.Serializers/Parquet/ParquetBuilder.cs
@@ -90,37 +90,47 @@ namespace PCAxis.Serializers
                 var variable = model.Meta.Variables[i];
                 if (variable.IsContentVariable && variable.Values.Count > 1)
                 {
-                    foreach (var value in variable.Values)
-                    {
-                        string columnName = $"{variable.Code}_{value.Code}";
-                        dataFields.Add(new DataField(columnName, typeof(double)));
-                        dataFields.Add(new DataField($"{columnName}_symbol", typeof(string))); // Add corresponding symbol column
-                    }
+                    ParquetBuilder.AddContentVariableFields(dataFields, variable);
                 }
                 else
                 {
-                    if (variable.IsContentVariable)
-                    {
-                        dataFields.Add(new DataField("value", typeof(double)));
-                        dataFields.Add(new DataField("value_symbol", typeof(string))); // Add corresponding symbol column
-                    }
-                    else
-                    {
-                        if (variable.IsTime)
-                        {
-                            // Add both the original time-value column and the timestamp column for time variables
-                            dataFields.Add(new DataField(variable.Name, typeof(string))); // Original time-value
-                            dataFields.Add(new DataField("timestamp", typeof(DateTime))); // Parsed timestamp
-                        }
-                        else
-                        {
-                            dataFields.Add(new DataField(variable.Name, typeof(string))); // Non-time variable
-                        }
-                    }
+                    ParquetBuilder.AddNonContentVariableFields(dataFields, variable);
                 }
             }
 
             return dataFields;
+        }
+
+        private static void AddContentVariableFields(List<DataField> dataFields, Variable variable)
+        {
+            foreach (var value in variable.Values)
+            {
+                string columnName = $"{variable.Code}_{value.Code}";
+                dataFields.Add(new DataField(columnName, typeof(double)));
+                dataFields.Add(new DataField($"{columnName}_symbol", typeof(string))); // Add corresponding symbol column
+            }
+        }
+
+        private static void AddNonContentVariableFields(List<DataField> dataFields, Variable variable)
+        {
+            if (variable.IsContentVariable)
+            {
+                dataFields.Add(new DataField("value", typeof(double)));
+                dataFields.Add(new DataField("value_symbol", typeof(string))); // Add corresponding symbol column
+            }
+            else
+            {
+                if (variable.IsTime)
+                {
+                    // Add both the original time-value column and the timestamp column for time variables
+                    dataFields.Add(new DataField(variable.Name, typeof(string))); // Original time-value
+                    dataFields.Add(new DataField("timestamp", typeof(DateTime))); // Parsed timestamp
+                }
+                else
+                {
+                    dataFields.Add(new DataField(variable.Name, typeof(string))); // Non-time variable
+                }
+            }
         }
 
 
@@ -146,64 +156,74 @@ namespace PCAxis.Serializers
 
                 if (variable.IsContentVariable && variable.Values.Count > 0)
                 {
-                    for (int j = 0; j < variable.Values.Count; j++)
-                    {
-                        string columnName;
-                        string symbolColumnName;
-
-                        if (variable.Values.Count > 1)
-                        {
-                            var value = variable.Values[j];
-                            columnName = $"{variable.Code}_{value.Code}";
-                            symbolColumnName = $"{columnName}_symbol"; // Corresponding symbol column name
-                        }
-                        else
-                        {
-                            columnName = "value";
-                            symbolColumnName = "value_symbol"; // Corresponding symbol column name
-                        }
-
-                        int columnIndex = dataFieldIndices[columnName];
-                        int symbolColumnIndex = dataFieldIndices[symbolColumnName]; // Get index of the symbol column
-                        int dataIndex = ParquetBuilder.GetDataIndex(index, variableValueCounts);
-
-                        if (dataIndex + j < data.Length)
-                        {
-                            dataIndex += j;
-                        }
-
-                        if (dataIndex >= 0 && dataIndex < data.Length)
-                        {
-                            if (dataSymbolMap.ContainsKey(data[dataIndex]))
-                            {
-                                row[symbolColumnIndex] = dataSymbolMap[data[dataIndex]]; // Set the symbol value
-                                row[columnIndex] = double.NaN; // Replace the value with double.NaN
-                            }
-                            else
-                            {
-                                row[columnIndex] = data[dataIndex];
-                                row[symbolColumnIndex] = null; // No symbol
-                            }
-                        }
-                    }
+                    PopulateContentVariableRow(index, variableValueCounts, data, row, dataFieldIndices, variable);
                 }
                 else
                 {
-                    var value = variable.Values[index[i]].Code;
-                    if (variable.IsTime)
-                    {
-                        value = variable.Values[index[i]].TimeValue;
-                        row[dataFieldIndices[variable.Name]] = value; // Original time-value
-                        row[dataFieldIndices["timestamp"]] = ParseTimeScale(value, variable.TimeScale); // Parsed timestamp
-                    }
-                    else
-                    {
-                        row[dataFieldIndices[variable.Name]] = value;
-                    }
+                    PopulateNonContentVariableRow(index, row, dataFieldIndices, variable, i);
                 }
             }
 
             return row;
+        }
+
+        private void PopulateContentVariableRow(int[] index, int[] variableValueCounts, double[] data, object[] row, Dictionary<string, int> dataFieldIndices, Variable variable)
+        {
+            for (int j = 0; j < variable.Values.Count; j++)
+            {
+                string columnName;
+                string symbolColumnName;
+
+                if (variable.Values.Count > 1)
+                {
+                    var value = variable.Values[j];
+                    columnName = $"{variable.Code}_{value.Code}";
+                    symbolColumnName = $"{columnName}_symbol"; 
+                }
+                else
+                {
+                    columnName = "value";
+                    symbolColumnName = "value_symbol"; // if there is only one Content
+                }
+
+                int columnIndex = dataFieldIndices[columnName];
+                int symbolColumnIndex = dataFieldIndices[symbolColumnName]; // Get index of the symbol column
+                int dataIndex = ParquetBuilder.GetDataIndex(index, variableValueCounts);
+
+                if (dataIndex + j < data.Length)
+                {
+                    dataIndex += j;
+                }
+
+                if (dataIndex >= 0 && dataIndex < data.Length)
+                {
+                    if (dataSymbolMap.ContainsKey(data[dataIndex]))
+                    {
+                        row[symbolColumnIndex] = dataSymbolMap[data[dataIndex]]; // Set the symbol value
+                        row[columnIndex] = double.NaN; // Replace the value with double.NaN
+                    }
+                    else
+                    {
+                        row[columnIndex] = data[dataIndex];
+                        row[symbolColumnIndex] = null; // No symbol
+                    }
+                }
+            }
+        }
+
+        private void PopulateNonContentVariableRow(int[] index, object[] row, Dictionary<string, int> dataFieldIndices, Variable variable, int i)
+        {
+            var value = variable.Values[index[i]].Code;
+            if (variable.IsTime)
+            {
+                value = variable.Values[index[i]].TimeValue;
+                row[dataFieldIndices[variable.Name]] = value; // Original time-value
+                row[dataFieldIndices["timestamp"]] = ParseTimeScale(value, variable.TimeScale); // Parsed timestamp
+            }
+            else
+            {
+                row[dataFieldIndices[variable.Name]] = value;
+            }
         }
 
 
@@ -216,69 +236,86 @@ namespace PCAxis.Serializers
         /// <exception cref="ArgumentException">Thrown if an invalid TimeScaleType or value is provided.</exception>
         private DateTime ParseTimeScale(string value, TimeScaleType timeScaleType)
         {
-
             // Check cache first
             if (parseCache.TryGetValue(value, out DateTime cachedDate))
             {
                 return cachedDate;
             }
 
+            DateTime parsedDate;
             switch (timeScaleType)
             {
                 case TimeScaleType.Annual:
-                    if (int.TryParse(value, out int year))
-                    {
-                        parseCache[value] = new DateTime(year, 1, 1);
-                        return parseCache[value];
-                    }
+                    parsedDate = ParseAnnual(value);
                     break;
-
                 case TimeScaleType.Halfyear:
-                    if (int.TryParse(value.Substring(0, 4), out int halfYearYear) && int.TryParse(value.Substring(4), out int halfYear))
-                    {
-                        int monthHalfyear = halfYear == 1 ? 1 : 7;  // For half year 1, it gives month 1 (January), for half year 2 it gives month 7 (July).
-                        parseCache[value] = new DateTime(halfYearYear, monthHalfyear, 1);
-                        return parseCache[value];
-                    }
+                    parsedDate = ParseHalfyear(value);
                     break;
-
                 case TimeScaleType.Quartely:
-                    if (int.TryParse(value.Substring(0, 4), out int quarterYear) && int.TryParse(value.Substring(4), out int quarter))
-                    {
-                        int monthQuarter = (quarter - 1) * 3 + 1;  // For quarter 1, this gives month 1 (January), for quarter 2 it gives 4 (April), and so on.
-                        parseCache[value] = new DateTime(quarterYear, monthQuarter, 1);
-                        return parseCache[value];
-                    }
+                    parsedDate = ParseQuarterly(value);
                     break;
-
                 case TimeScaleType.Monthly:
-                    if (int.TryParse(value.Substring(0, 4), out int monthYear) && int.TryParse(value.Substring(4, 2), out int month))
-                    {
-                        parseCache[value] = new DateTime(monthYear, month, 1);
-                        return parseCache[value];
-                    }
+                    parsedDate = ParseMonthly(value);
                     break;
-
-                // This time ISO-8601 compliant.
                 case TimeScaleType.Weekly:
-                    if (int.TryParse(value.Substring(0, 4), out int weekYear) && int.TryParse(value.Substring(4), out int week))
-                    {
-                        DateTime jan1 = new DateTime(weekYear, 1, 1);
-                        // Find the first Thursday of the year
-                        DateTime firstThursday = jan1.AddDays((DayOfWeek.Thursday + 7 - jan1.DayOfWeek) % 7);
-                        // Calculate the start date of the first week
-                        DateTime firstWeekStart = firstThursday.AddDays(-(int)firstThursday.DayOfWeek + (int)DayOfWeek.Monday);
-                        // Calculate the start date of the desired week
-                        DateTime weekStartDate = firstWeekStart.AddDays((week - 1) * 7);
-
-                        parseCache[value] = weekStartDate;
-                        return parseCache[value];
-                    }
+                    parsedDate = ParseWeekly(value);
                     break;
-
+                default:
+                    throw new ArgumentException("Invalid TimeScaleType or value.");
             }
 
-            throw new ArgumentException("Invalid TimeScaleType or value.");
+            parseCache[value] = parsedDate;
+            return parsedDate;
+        }
+
+        private static DateTime ParseAnnual(string value)
+        {
+            if (int.TryParse(value, out int year))
+            {
+                return new DateTime(year, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            }
+            throw new ArgumentException("Invalid value for Annual TimeScaleType.");
+        }
+
+        private static DateTime ParseHalfyear(string value)
+        {
+            if (int.TryParse(value.Substring(0, 4), out int halfYearYear) && int.TryParse(value.Substring(4), out int halfYear))
+            {
+                int monthHalfyear = halfYear == 1 ? 1 : 7;
+                return new DateTime(halfYearYear, monthHalfyear, 1, 0, 0, 0, DateTimeKind.Utc);
+            }
+            throw new ArgumentException("Invalid value for Halfyear TimeScaleType.");
+        }
+
+        private static DateTime ParseQuarterly(string value)
+        {
+            if (int.TryParse(value.Substring(0, 4), out int quarterYear) && int.TryParse(value.Substring(4), out int quarter))
+            {
+                int monthQuarter = (quarter - 1) * 3 + 1;
+                return new DateTime(quarterYear, monthQuarter, 1, 0, 0, 0, DateTimeKind.Utc);
+            }
+            throw new ArgumentException("Invalid value for Quarterly TimeScaleType.");
+        }
+
+        private static DateTime ParseMonthly(string value)
+        {
+            if (int.TryParse(value.Substring(0, 4), out int monthYear) && int.TryParse(value.Substring(4, 2), out int month))
+            {
+                return new DateTime(monthYear, month, 1, 0, 0, 0, DateTimeKind.Utc);
+            }
+            throw new ArgumentException("Invalid value for Monthly TimeScaleType.");
+        }
+
+        private static DateTime ParseWeekly(string value)
+        {
+            if (int.TryParse(value.Substring(0, 4), out int weekYear) && int.TryParse(value.Substring(4), out int week))
+            {
+                DateTime jan1 = new DateTime(weekYear, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                DateTime firstThursday = jan1.AddDays((DayOfWeek.Thursday + 7 - jan1.DayOfWeek) % 7);
+                DateTime firstWeekStart = firstThursday.AddDays(-(int)firstThursday.DayOfWeek + (int)DayOfWeek.Monday);
+                return firstWeekStart.AddDays((week - 1) * 7);
+            }
+            throw new ArgumentException("Invalid value for Weekly TimeScaleType.");
         }
 
         /// <summary>
